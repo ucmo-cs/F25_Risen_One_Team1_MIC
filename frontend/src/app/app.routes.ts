7755a607bd6d44ebcc78b65ff89d79795253bf32
Christian <cjv68170@ucmo.edu>
2025-02-03 14:52:35 -0600

    timesheet component + timesheet tab

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,21 +1,22 @@
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { NgModel } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { TimesheetComponent } from './components/timesheet/timesheet.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  ];
+  { path: "timesheet", component: TimesheetComponent },
+];
 
-  @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule],
-    providers: []
-  })
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: []
+})
 
-  export class AppRoutingModule{
-    
-  }
+export class AppRoutingModule {
+
+}
