564882ad837f3aa2fcfedf8815e72a164a736f90
UltimateGG <clubblock1@gmail.com>
2025-03-06 23:39:10 -0600

    Merge

---
+++ frontend/src/app/services/project.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Project } from '@shared/types';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProjectService {
+  constructor(private http: HttpClient) {}
+
+  public getProjects() {
+    return this.http.get(environment.apiGatewayUrl + '/projects');
+  }
+
+  public updateProjects(projects: Project[]) {
+    return this.http.post(
+      environment.apiGatewayUrl + '/projects/update',
+      projects
+    );
+  }
+}
