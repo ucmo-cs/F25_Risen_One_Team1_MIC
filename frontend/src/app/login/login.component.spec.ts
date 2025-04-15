910e147731cce1befa625d4beef4b6209f275c62
UltimateGG <clubblock1@gmail.com>
2025-04-14 21:56:21 -0500

    Fix login test

--- frontend/src/app/login/login.component.spec.ts
+++ frontend/src/app/login/login.component.spec.ts
@@ -1,4 +1,14 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+  waitForAsync,
+} from '@angular/core/testing';
+import {
+  provideHttpClientTesting,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
 import { LoginComponent } from './login.component';
 import { AppModule } from '../app.module';
@@ -6,15 +16,18 @@ import { AppModule } from '../app.module';
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let httpMock: HttpTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppModule],
       declarations: [LoginComponent],
+      providers: [provideHttpClientTesting(), LoginComponent],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
+    httpMock = TestBed.inject(HttpTestingController);
     fixture.detectChanges();
     await fixture.whenStable();
   });
@@ -30,15 +43,20 @@ describe('LoginComponent', () => {
   });
 
   it('Should have disabled login button at first', () => {
+    fixture.detectChanges();
+
     const loginBtn = (fixture.nativeElement as HTMLElement).querySelector(
       'button'
     ) as HTMLButtonElement;
+
     expect(loginBtn.textContent).toBe('Login');
-    console.log(loginBtn);
     expect(loginBtn.disabled).toBeTrue();
   });
 
-  it('Should login', () => {
+  it('Should mock login', () => {
+    const username = 'test.User!';
+    const password = '!#passw0Rd-';
+
     const usernameField = document.getElementById(
       'username'
     ) as HTMLInputElement;
@@ -48,27 +66,37 @@ describe('LoginComponent', () => {
     expect(usernameField).not.toBeNull();
     expect(passwordField).not.toBeNull();
 
-    usernameField.value = 'admin';
+    usernameField.value = username;
     usernameField.dispatchEvent(new Event('input'));
     fixture.detectChanges();
 
-    expect(component.username).toBe('admin');
+    expect(component.username).toBe(username);
 
-    passwordField.value = 'password';
+    passwordField.value = password;
     passwordField.dispatchEvent(new Event('input'));
     fixture.detectChanges();
 
-    expect(component.password).toBe('password');
+    expect(component.password).toBe(password);
 
     const loginBtn = (fixture.nativeElement as HTMLElement).querySelector(
       'button'
     ) as HTMLButtonElement;
     expect(loginBtn.disabled).toBeFalse();
 
-    loginBtn.dispatchEvent(new Event('click'));
+    loginBtn.click();
     fixture.detectChanges();
 
     expect(component.isLoading).toBeTrue();
     expect(loginBtn.disabled).toBeTrue();
+
+    const req = httpMock.expectOne(
+      (req) => req.url.includes('/dev/login') && req.method === 'POST'
+    );
+
+    expect(req.request.body).toEqual({ username, password });
+    req.flush({ message: 'Login successful' });
+
+    fixture.detectChanges();
+    expect(component.isLoading).toBeFalse();
   });
 });
